637ffe11df082770edd8e71c57cba786444d251d
Daniel Moshayof <43912232+dmoshayof@users.noreply.github.com>
2021-07-10 21:56:00 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ $(document).on('click', function(){
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("July 16, 2021 00:00:00").getTime();
+var countDownDate = new Date("September 10, 2021 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
