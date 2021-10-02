efde9e1f2595617212a824c6c510c1fcb258da2a
Daniel Moshayof <43912232+dmoshayof@users.noreply.github.com>
2021-10-02 12:40:32 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -37,6 +37,7 @@
         x.play();
         if (image.src.match("assets/img/mute.png")) {
             image.src = "assets/img/volume.png";
+            x.play();
         }
         else {
             image.src = "assets/img/mute.png";
@@ -49,7 +50,7 @@
 
 
 // Set the date we're counting down to
-var countDownDate = new Date("September 10, 2021 00:00:00").getTime();
+var countDownDate = new Date("November 12, 2021 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
