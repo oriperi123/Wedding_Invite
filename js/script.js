fd27d8c5f45f5283a825c5c5d1211982b91740f7
Daniel Moshayof <43912232+dmoshayof@users.noreply.github.com>
2021-10-12 21:59:21 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -31,8 +31,7 @@
       }
   }
 
-  $(document).ready(
-      function(){
+  $(document).ready(function(){
     $(".musicOff").click(function(){
         var image = document.getElementById('myImg');
         var x = document.getElementById("my_audio");
